79d5b401446278fec42955ba070fc9e970cd904d
Siarhei Kandratsyeu <s.kandratsyeu@gmail.com>
2021-01-08 14:05:09 +0300

    link to resume

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById('downloadResume')
+    button.addEventListener('click', () => {
+        window.open('https://drive.google.com/file/d/189jtNN0OlVcFc_5VNWGFa4hyWDvpFNo-/view?usp=sharing', '_blank')
+    })
+})
